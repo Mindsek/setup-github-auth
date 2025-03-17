f2fd43d9429b90756a4ab3df5d7881fce1bec607
Timber <contactmindsek@gmail.com>
2025-03-17 20:37:08 +0100

    refactor: install next-auth, refactor next config

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
